76725ab2658c61d8cc9ec7b4e7055b2476298974
MoonWalker <silavsale@gmail.com>
2024-01-21 22:29:23 -0500

    add 01 Basic - 05 Max number

--- src/01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ src/01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,7 +1,7 @@
 function findMaxNumber(arr) {
-  let largestNumber = 0;
+  let largestNumber = arr[0];
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     if (arr[i] > largestNumber) {
       largestNumber = arr[i];
     }
